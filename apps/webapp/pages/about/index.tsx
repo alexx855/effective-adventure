4dbab1732d3f212d84dcb594f29b0d632f96fee2
Alex Pedersen <info@alexpedersen.net>
2022-04-04 12:22:29 +0200

    generate demo pages, link routes

---
+++ apps/webapp/pages/about/index.tsx
@@ -0,0 +1,16 @@
+// import styles from './index.module.css';
+
+/* eslint-disable-next-line */
+export interface AboutProps {}
+
+export function About(props: AboutProps) {
+  return (
+    <>
+      <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <span>Welcome to About!</span>
+      </h1>
+    </>
+  );
+}
+
+export default About;
---
+++ apps/webapp/pages/hive/index.tsx
@@ -0,0 +1,16 @@
+// import styles from './index.module.css';
+
+/* eslint-disable-next-line */
+export interface HiveProps {}
+
+export function Hive(props: HiveProps) {
+  return (
+    <>
+      <h1 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+        <span>Welcome to Hive!</span>
+      </h1>
+    </>
+  );
+}
+
+export default Hive;
