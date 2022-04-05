ed318d5a427f9bbb0ae9a90e05b3076ff8230217
Alex Pedersen <info@alexpedersen.net>
2022-04-05 15:56:01 +0200

    add some images, test deploy env

--- apps/webapp/pages/_app.tsx
+++ apps/webapp/pages/_app.tsx
@@ -11,6 +11,10 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <main className="app">
         <Component {...pageProps} />
       </main>
+
+      <div className="absolute bottom-0 right-0 z-99 pb-2 pr-2">
+        <p id="release-version" className='text-right'>{process.env.NEXT_PUBLIC_RELEASE_VERSION || 'NEXT_PUBLIC_RELEASE_VERSION'}</p>
+      </div>
     </>
   );
 }
--- apps/webapp/pages/index.tsx
+++ apps/webapp/pages/index.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
 // import confetti from 'https://cdn.skypack.dev/canvas-confetti'
 // import { useEffect } from 'react'
 
@@ -8,37 +10,52 @@ export function Index() {
   // })
 
   console.log(process.env);
+  console.log('hello next.js');
   console.log(process.env.NEXT_PUBLIC_ANALYTICS_ID);
+  console.log(process.env.RELEASE_VERSION);
   console.log(process.env.branch);
   console.log(process.env.hash);
 
-  console.log('hello next.js');
-
   return (
-    <div className="bg-gray-50 h-screen w-screen overflow-hidden">
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-          <span className="block">🐝 BEE supportive,</span>
-          <span className="block text-indigo-600">, help real people </span>
-        </h2>
-        <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-          <div className="inline-flex rounded-md shadow">
-            <Link href="/hive/">
-              <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
-                Hive
-              </a>
-            </Link>
-          </div>
-          <div className="ml-3 inline-flex rounded-md shadow">
-            <Link href="/about/">
-              <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">
-                About
-              </a>
-            </Link>
+    <div className="h-screen w-screen overflow-hidden">
+      <div id="hero" className="bg-gray-50">
+        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            <Image
+              className="inline"
+              alt="honeypot"
+              src="/bee/SVG/full.svg"
+              height={40}
+              width={40}
+              layout="fixed"
+            />
+            <span className="inline pl-2">BEE supportive</span>
+            {/* <span className="block text-indigo-600">helping real people in real time</span> */}
+          </h2>
+          <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+            <div className="inline-flex rounded-md shadow">
+              <Link href="/hive/">
+                <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+                  Hive
+                </a>
+              </Link>
+            </div>
+            <div className="ml-3 inline-flex rounded-md shadow">
+              <Link href="/about/">
+                <a className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">
+                  About
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
 
+      <div className="h-48 w-full absolute inset-x-0 bottom-0 z-1 overflow-hidden">
+        <div className='top-20 relative w-full h-full'>
+            <Image alt="honeypot" src="/pot/SVG/full.svg" layout="fill" />
+        </div>
+      </div>
     </div>
   );
 }
