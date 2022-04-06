03c6a492660b5f53fd013ac04bbb200fd75498ed
Alex Pedersen <info@alexpedersen.net>
2022-04-06 17:38:53 +0200

    test deploy

--- apps/webapp/pages/_app.tsx
+++ apps/webapp/pages/_app.tsx
@@ -1,19 +1,35 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Link from 'next/link';
+
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to webapp!</title>
+        <title>Welcome to effective adventure!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
       </main>
 
       <div className="absolute bottom-0 right-0 z-99 pb-2 pr-2">
-        <p id="release-version" className='text-right'>{process.env.NEXT_PUBLIC_RELEASE_VERSION || 'NEXT_PUBLIC_RELEASE_VERSION'}</p>
+        <p id="release-info" className="text-right">
+          {process.env.NEXT_PUBLIC_VERSION || 'development'}{' '}
+          {process.env.NEXT_PUBLIC_IPFS_CID && (
+            <Link href="https://ipfs-effective-adventure.alexpedersen.dev/">
+              <a
+                id="ipfs-cid"
+                className="text-right"
+                target="_blank"
+                rel="noreferrer"
+              >
+                IPFS CID {process.env.NEXT_PUBLIC_IPFS_CID}
+              </a>
+            </Link>
+          )}
+        </p>
       </div>
     </>
   );
